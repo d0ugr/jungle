a7e27e38aa6fb4baa73f04c43ca39a21e2edcc42
d0ugr <doug@escm.ml>
2020-05-02 16:21:08 -0400

    Refactor line item products lookup

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,7 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @line_items = @order.line_items
-    @line_item_products = @line_items.map { |item|
-      Product.find(item[:product_id])
-    }
+    @line_item_products = Product.where(id: @line_items.map { |item| item.product_id })
   end
 
   def create
