435ae213522e6f6b5457a0e8fccc00eca7aeebbe
d0ugr <doug@escm.ml>
2020-05-03 18:43:26 -0400

    Add Admin Categories create route

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,6 +11,13 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
   private
