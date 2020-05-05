d346ac31df7e5d5d9fb05fd313eb3b0e7919cbf1
d0ugr <doug@escm.ml>
2020-05-05 14:23:46 -0400

    Add initial User validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,42 @@ RSpec.describe User, type: :model do
 
   subject do
     described_class.new(
-      name:     "Test User",
-      email:    "test@test.com",
-      password: "secret"
+      name:                  "Test User",
+      email:                 "test@test.com",
+      password:              "secret",
+      password_confirmation: "secret"
     )
   end
 
   describe "Validations" do
 
+    it "valid with valid values" do
+      expect(subject).to be_valid
+    end
 
+    it "invalid with missing first name" do
+    end
+
+    it "invalid with missing last name" do
+    end
+
+    it "invalid with missing email" do
+    end
+
+    it "invalid with missing password" do
+    end
+
+    it "invalid with missing password confirmation" do
+    end
+
+    it "invalid with mismatch passwords" do
+    end
+
+    it "invalid with short password" do
+    end
+
+    it "invalid with existing case insensitive email" do
+    end
 
   end
 
