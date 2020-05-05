67f540bbb2acc7ffd77a08ec3ef97246bdbc0ab8
d0ugr <doug@escm.ml>
2020-05-05 03:13:45 -0400

    Add Product test expectations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,15 +5,23 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     it 'name is required' do
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include(["Name can't be blank"])
     end
 
     it 'price is required' do
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'quantity is required' do
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'category is required' do
+      expect(@product).to be_invalid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
   end
