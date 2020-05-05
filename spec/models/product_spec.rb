effb18ef558d395720b629cdb7bf611a5aab3813
d0ugr <doug@escm.ml>
2020-05-05 03:16:49 -0400

    Add Product test code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,21 +5,52 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     it 'name is required' do
+      @category = Category.new(name: "Stuff")
+      @product = Product.new(
+        name:     nil,
+        price:    4.20,
+        quantity: 1000000,
+        category: @category
+      )
+      @product.save
       expect(@product).to be_invalid
-      expect(@product.errors.full_messages).to include(["Name can't be blank"])
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'price is required' do
+      @category = Category.new(name: "Stuff")
+      @product = Product.new(
+        name:     "HUMAN FEET SHOES",
+        price:    nil,
+        quantity: 1000000,
+        category: @category
+      )
+      @product.save
       expect(@product).to be_invalid
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'quantity is required' do
+      @category = Category.new(name: "Stuff")
+      @product = Product.new(
+        name:     "HUMAN FEET SHOES",
+        price:    4.20,
+        quantity: nil,
+        category: @category
+      )
       expect(@product).to be_invalid
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'category is required' do
+      @category = Category.new(name: "Stuff")
+      @product = Product.new(
+        name:     "HUMAN FEET SHOES",
+        price:    4.20,
+        quantity: 1000000,
+        category: nil
+      )
+      @product.save
       expect(@product).to be_invalid
       expect(@product.errors.full_messages).to include("Category can't be blank")
     end
