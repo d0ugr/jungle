8f6c9722f1ed3cff83437dd8bf7c10667637eda3
d0ugr <doug@escm.ml>
2020-05-05 15:00:43 -0400

    Update users to include first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200503235435) do
+ActiveRecord::Schema.define(version: 20200505185414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,9 +57,10 @@ ActiveRecord::Schema.define(version: 20200503235435) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
