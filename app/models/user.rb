7eb15062e8d3ef0867534967964ec7d7d9f4edcc
d0ugr <doug@escm.ml>
2020-05-05 16:16:23 -0400

    Add User.authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ActiveRecord::Base
   validates :first_name,            presence: true
   validates :last_name,             presence: true
 
+  def self.authenticate_with_credentials(email, password)
+
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+
+  end
+
 end
